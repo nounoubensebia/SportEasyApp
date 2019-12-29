f5d7452c473ab546cf5302d8df360cb2b8539f7a
nounoubensebia <nounoubensebia96@gmail.com>
2019-12-29 18:21:18 +0100

    skeleton

---
+++ src/main/java/com/infra/infra/contollers/GroupeControllerHtml.java
@@ -0,0 +1,41 @@
+package com.infra.infra.contollers;
+
+import com.infra.infra.models.Groupe;
+import com.infra.infra.services.GroupeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+
+@Controller
+public class GroupeControllerHtml {
+    private GroupeRepository groupeRepository;
+
+    @Autowired
+    public void setGroupeRepository(GroupeRepository groupeRepository) {
+        this.groupeRepository = groupeRepository;
+    }
+
+    //Requestmapping pour définir la route
+    @RequestMapping("/groupshtml")
+    public String showGroupes(Model model)
+    {
+
+        ArrayList<Groupe> groupes = new ArrayList<>();
+
+        for (Groupe groupe:groupeRepository.findAll())
+        {
+            groupes.add(groupe);
+        }
+
+        //Il faut ajouter les données à afficher à l'HTML
+        model.addAttribute("groupes",groupes);
+
+        // il faut retourner le nom du fichier HTML à afficher
+        return "groupes";
+    }
+
+}
---
+++ src/main/java/com/infra/infra/services/GroupeRepository.java
@@ -0,0 +1,7 @@
+package com.infra.infra.services;
+
+import com.infra.infra.models.Groupe;
+import org.springframework.data.repository.CrudRepository;
+
+public interface GroupeRepository extends CrudRepository<Groupe,Long> {
+}
