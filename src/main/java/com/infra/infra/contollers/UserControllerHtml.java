71a9b33a75d399a0da3203fd5684f301dbcc4b01
nassim <lamarinassim31@gmail.com>
2019-12-31 16:39:49 +0100

    inscription is OK reste afaire les TODO pour rendre le truc plus User friendly

--- src/main/java/com/infra/infra/contollers/UserControllerHtml.java
+++ src/main/java/com/infra/infra/contollers/UserControllerHtml.java
@@ -7,6 +7,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.sql.Date;
+
+
 @Controller
 public class UserControllerHtml {
 
@@ -15,20 +18,38 @@ public class UserControllerHtml {
     public void setUserService(UserService userService) {
         this.userService = userService;
     }
+
+    //Methodes Tester
+    //TODO : metre email en rouge et ne pas suprimer les autres champs du formulaire+ affichage que mail existe deja
     @GetMapping("/first-inscription")
     public String requestFirstInscription(Model model){return "inscription";}
 
     @PostMapping("/first-inscription")
-    public  String postFirstInscription(@RequestParam("email") String email){
-        /*
-        if (userService.checkEmail(mail)){//ce mail est déja USED
-            model.addAttribute("checkMail", "The user "+mail+" already exists !!");
-        }else {
-            userService.create(personForm);
-            model.addAttribute(personForm);
+    public  String postFirstInscription(
+            @RequestParam("email") String email,
+            @RequestParam("lname") String lname,
+            @RequestParam("fname") String fname,
+            @RequestParam("Sex") String sex,
+            @RequestParam("psw") String psw,
+            @RequestParam("date") Date date,
+            Model model){
 
-        }*/
-        System.out.println(email);
-        return "userInfo";
+        if (userService.checkEmail(email)){//ce mail est déja USED
+            model.addAttribute("checkMail", "The user "+email+" already exists !!");
+            return "inscription";
+        }else {
+            User personForm =new User();
+            personForm.setLastName(lname);
+            personForm.setFirstName(fname);
+            personForm.setBirthDate(date);
+            personForm.setGender(Integer.parseInt(sex));
+            personForm.setPassword(psw);
+            personForm.setEmail(email);
+            userService.create(personForm);
+
+            return "login";
+
+        }
+
     }
 }
