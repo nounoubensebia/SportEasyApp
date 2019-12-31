b363c46e4196b9a8684fa2a45d44d25aa5154a2e
Paul <p.vong@hotmail.fr>
2019-12-31 14:24:32 +0100

    affichage des activités: ajout fichier html et controller

---
+++ src/main/java/com/infra/infra/contollers/SessionControllerHtml.java
@@ -0,0 +1,34 @@
+package com.infra.infra.contollers;
+
+import com.infra.infra.models.Session;
+import com.infra.infra.services.session.SessionRepository;
+import com.infra.infra.services.session.SessionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+public class SessionControllerHtml {
+    private SessionService sessionService;
+
+    @Autowired
+    public void setSessionService(SessionService sessionService) {
+        this.sessionService = sessionService;
+    }
+
+    //Requestmapping pour définir la route
+    @RequestMapping("/listeActivites")
+    public String showSession(Model model)
+    {
+        List<Session> listeActivites = sessionService.getAll();
+
+        //Il faut ajouter les données à afficher à l'HTML
+        model.addAttribute("listeActivites",listeActivites);
+
+        // il faut retourner le nom du fichier HTML à afficher
+        return "listeActivites";
+    }
+}
